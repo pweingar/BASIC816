d652d7881e5df72f181005ab82d2c8183d1e58e6
Raymond Wiker <rwiker@gmail.com>
2021-03-19 13:51:35 +0100

    Corrections and improvements; additional comments; new implementation of tan().

--- src/transcendentals.s
+++ src/transcendentals.s
@@ -1,12 +1,24 @@
-;
-; POLY_HR - Horner's rule
-;
-; On entry: as, xl
-; X points to coefficient table of 5 values
-; ARGUMENT1 contains a floating-point argument (which may be x,
-;   or x*x, depending on the actual function being approximated.)
-; Leaves the value in ARGUMENT1
-POLY_HR         .proc
+;;; Transcendentals for the C256Foenix, using the floating-point
+;;; coprocessor.
+;;;
+;;; Conventions:
+;;; - Functions that are named Q_something do little or no error
+;;; checking or setup. It is up to the caller to ensure these
+;;; functions are called with sensible parameter values.
+;;; - Parameters are passed in ARGUMENT1 (and possibly ARGUMENT2). It
+;;; is up to the caller to ensure that the parameters have been
+;;; converted into the right type.
+
+;;;
+;;; Q_POLY_HR - Horner's rule
+;;;
+;;; On entry: as, xl
+;;; X points to coefficient table of 5 values. These are stored in the
+;;; reverse order.
+;;; ARGUMENT1 contains a floating-point argument (which may be x,
+;;; or x*x, depending on the actual function being approximated.)
+;;; Leaves the value in ARGUMENT1
+Q_POLY_HR       .proc
                 PHY
                 setas
                 ;; Set up adder for input from input muxes and ADD operation.
@@ -20,11 +32,12 @@ POLY_HR         .proc
                 STA @l FP_MATH_INPUT0_LL
                 LDA 2,X
                 STA @l FP_MATH_INPUT0_HL
-                LDY #4          ; 5 coefficients.
-loop            INX
+                LDY #4          ; 4 more coefficients.
+loop            INX             ; point to the next coefficient
                 INX
                 INX
                 INX
+;;; Multiply by x
                 setas
                 LDA #FP_OUT_MULT
                 STA @l FP_MATH_CTRL1
@@ -36,12 +49,15 @@ loop            INX
                 NOP
                 NOP
                 NOP
+;;; Retrieve product and store it back into input0. Note that all
+;;; bytes of the output must be retrieved before we change the input.
                 LDA @l FP_MATH_OUTPUT_FP_LL
                 PHA
                 LDA @l FP_MATH_OUTPUT_FP_HL
                 STA FP_MATH_INPUT0_HL
                 PLA
                 STA @l FP_MATH_INPUT0_LL
+;;; Get next coefficient and add it to partial result
                 setas
                 LDA #FP_OUT_ADD
                 STA @l FP_MATH_CTRL1
@@ -55,11 +71,16 @@ loop            INX
                 NOP
                 DEY
                 BEQ done
+;;; Get result of addition and send it back to input0. As before, note
+;;; that all bytes of the output must be retrieved before we change
+;;; the input.
                 LDA @l FP_MATH_OUTPUT_FP_LL
-                STA @l FP_MATH_INPUT0_LL
+                PHA
                 LDA @l FP_MATH_OUTPUT_FP_HL
                 STA @l FP_MATH_INPUT0_HL
-                BRL loop
+                PLA
+                STA @l FP_MATH_INPUT0_LL
+                BRA loop
 done            LDA @l FP_MATH_OUTPUT_FP_LL
                 STA @l ARGUMENT1
                 LDA @l FP_MATH_OUTPUT_FP_HL
@@ -68,6 +89,9 @@ done            LDA @l FP_MATH_OUTPUT_FP_LL
                 RTS
                 .pend
 
+;;; Q_SQ - "Quick" squaring function.
+;;; ARGUMENT1 contains a floating-point argument.
+;;; Leaves the value in ARGUMENT1
 Q_SQ            .proc
                 setas
                 LDA #0
@@ -91,14 +115,47 @@ Q_SQ            .proc
                 RTS
                 .pend
 
-;;; "quick" scaling function: as long as ARGUMENT1 < ARGUMENT2, place
-;;; ARGUMENT1 in FP input 0 of fp copro, read out result, store back
-;;; in ARGUMENT1. Return the number of times through the loop in X.
+;;; Q_INV - "Quick" inverse.
+;;; ARGUMENT1 contains a floating-point argument.
+;;; Leaves the value in ARGUMENT1
+Q_INV           .proc
+                setas
+                LDA #0
+                STA @l FP_MATH_CTRL0
+                LDA #FP_OUT_DIV
+                STA @l FP_MATH_CTRL1
+                setal
+                LDA ARGUMENT1
+                STA @l FP_MATH_INPUT1_LL
+                LDA @l ARGUMENT1+2
+                STA @l FP_MATH_INPUT1_HL
+                LDA @l fp_one
+                STA @l FP_MATH_INPUT0_LL
+                LDA @l fp_one+2
+                STA @l FP_MATH_INPUT0_HL
+                NOP
+                NOP
+                NOP
+                LDA @l FP_MATH_OUTPUT_FP_LL
+                STA ARGUMENT1
+                LDA @l FP_MATH_OUTPUT_FP_HL
+                STA ARGUMENT1+2
+                RTS
+                .pend
+
+;;; Q_FP_SCALE - "quick" scaling function:
+;;;
+;;; As long as ARGUMENT1 < ARGUMENT2, place ARGUMENT1 in FP input 0 of
+;;; fp copro, read out result, store back in ARGUMENT1. Return the
+;;; number of times through the loop in X.
+;;;
 ;;; Assumes that ARGUMENT2 has already been copied to fp input 1 of
 ;;; the float coprocessor, and that the float coprocessor has been
 ;;; setup for the appropriate operation.
+;;;
 ;;; Also assumes that both A and index registers are setup for 16-bit
 ;;; operation.
+
 Q_FP_SCALE      .proc
                 .al
                 .xl
@@ -125,33 +182,53 @@ done
                 RETURN
                 .pend
 
+;;; Q_QP_SCALE_TAU: scale ARGUMENT1 to [0, 2pi]
+;;; - if ARGUMENT1 is negative, use the absolue value and subtract the
+;;; result from 2pi at the end.
+;;; - implemented as a loop that repeatedly subtracts 2pi until the
+;;; result is less than 2pi.
+;;; Note: this is not fast or accurate when the argument is large. It
+;;; could be made slightly more accurate and faster by runing several
+;;; loops using (e.g.) 1000*2pi, 100*2pi, 10*2pi and 2pi. It would
+;;; still lose accuracy, and maybe we shouldn't encourage people to
+;;; try to run sin(31415.927)...
 Q_FP_SCALE_TAU  .proc
+                ;; check if ARGUMENT1 is negative
                 setas
                 LDA ARGUMENT1+3
                 BPL notneg
+                ;; Yes; flip sign bit...
                 AND #$7F
                 STA ARGUMENT1+3
+                ;; and push 1 to indicate that the original value was
+                ;; negative
                 LDA #1
                 PHA
                 BRA compute
-notneg          LDA #0
+notneg          LDA #0          ; not negative, so push 0.
                 PHA
+                ;; set up fp copro to subtract input1 from input0
 compute         LDA #FP_ADD_IN0_MUX0 | FP_ADD_IN1_MUX1
                 STA @l FP_MATH_CTRL0
                 LDA #FP_OUT_ADD
                 STA @l FP_MATH_CTRL1
                 setaxl
+                ;; write 2pi to input1, and to ARGUMENT2.
                 LDA @l twopi
                 STA ARGUMENT2
                 STA @l FP_MATH_INPUT1_LL
                 LDA @l twopi+2
                 STA ARGUMENT2+2
                 STA @l FP_MATH_INPUT1_HL
+                ;; make Q_FP_SCALE do the actual work.
                 CALL Q_FP_SCALE
                 setas
+                ;; Get the argument-was-negative flag from stack.
                 PLA
                 setal
                 BEQ done
+                ;; the argument was negative, so we compute (2pi-x) by
+                ;; subtracting 2i from x, then negating the result.
                 LDA ARGUMENT1
                 STA @l FP_MATH_INPUT0_LL
                 LDA ARGUMENT1+2
@@ -162,18 +239,40 @@ compute         LDA #FP_ADD_IN0_MUX0 | FP_ADD_IN1_MUX1
                 LDA @l FP_MATH_OUTPUT_FP_LL
                 STA ARGUMENT1
                 LDA @l FP_MATH_OUTPUT_FP_HL
+                ;; flip sign bit before saving top 16 bits of result.
                 AND #$7fff
                 STA ARGUMENT1+2
 done
                 RETURN
                 .pend
 
+;;; Q_FP_NORM_ANGLE - normalize angle.
+;;; - if angle is greater than pi, subtract it from 2pi and record the
+;;; fact in flag bit 2.
+;;; - if angle is greater than pi/2, subtract it from pi and record
+;;; the fact in flag bit 1.
+;;; - if angle is greater than pi/4, subtract it from pi and record
+;;; the fact in flag bit 0.
+;;; At the end, the angle will be in the range [0,pi/4), and the flags
+;;; will indicate
+;;; 0: the "opposite" function must be called (is, sin() for cos(),
+;;; and vice versa).
+;;; 1: the parameter has been reflected around pi/2, so the value for
+;;; cos() must be negated.
+;;; 2: the parameter has been reflected around pi, so the value for
+;;; sin() must be negated.
+;;; Note that the order of the constants twopi, onepi, halfpi and
+;;; quarterpi (at the end of this file) is important, and allows us to
+;;; use a simple loop instead of a long, linear sequence of operations.
 Q_FP_NORM_ANGLE .proc
                 .al
                 .xl
                 PHY
                 LDX #0
                 LDY #0
+                ;; 32-bit comparison, using CMP and SBC. Using CMP for
+                ;; the first subtraction means that we do not have to
+                ;; initialize the carry bit.
 loop            LDA ARGUMENT1
                 CMP @l onepi,x
                 LDA ARGUMENT1+2
@@ -194,22 +293,28 @@ loop            LDA ARGUMENT1
                 STA ARGUMENT1
                 LDA @l FP_MATH_OUTPUT_FP_HL
                 STA ARGUMENT1+2
-                SEC
-less            TYA
-                ROL
-                TAY
+                SEC             ; set carry to indicate a reflection
+less            TYA             ; carry already cleared if we branched
+                                ; from the comparison.
+                ROL             ; shift carry into flags...
+                TAY             ; and store back into y
+                INX             ; next set of values
                 INX
                 INX
                 INX
-                INX
-                CPX #12
+                CPX #12         ; check if we have already looked at 3
+                                ; sets of values (or 12 bytes).
                 BNE loop
-                TYA
+                TYA             ; copy Y to X, as that's what we have
+                                ; said that we want tro return the
+                                ; value in.
                 TAX
                 PLY
                 RETURN
                 .pend
 
+;;; FP_SCALE: body of BASIC function "SCALETAU". Will be removed at
+;;; some point, but has been useful for testing.
 FP_SCALE        .proc
                 PHP
                 setaxl
@@ -224,6 +329,9 @@ FP_SCALE        .proc
 
 ;;; "quick" cosine computation, assuming that the argument is in the
 ;;; range [0, pi/4].
+;;; Implemented via Horner's rule, using even powers of x... so we
+;;; start by computing x^2, so that we can drop computing 0.0 * odd
+;;; powers of x.
 Q_FP_COS        .proc
                 PHP
                 setaxl
@@ -237,7 +345,7 @@ Q_FP_COS        .proc
                 PLB
                 setal
                 LDX #<>cos_coeff
-                CALL POLY_HR
+                CALL Q_POLY_HR
                 LDA #TYPE_FLOAT
                 STA @l ARGTYPE1
                 PLB
@@ -249,6 +357,10 @@ Q_FP_COS        .proc
 
 ;;; "quick" sine computation, assuming that the argument is in the
 ;;; range [0, pi/4]
+;;; Implemented via Horner's rule, using even powers of x... so we
+;;; start by computing x^2, so that we can drop computing 0.0 * odd
+;;; powers of x. At the end, we multiply the result by x, since sine
+;;; actually has a Taylor expansion that uses odd powers of x.
 Q_FP_SIN        .proc
                 PHP
                 setaxl
@@ -266,7 +378,7 @@ Q_FP_SIN        .proc
                 PLB
                 setal
                 LDX #<>sin_coeff
-                CALL POLY_HR
+                CALL Q_POLY_HR
                 PLB
                 CALL OP_FP_MUL
                 PLX
@@ -281,26 +393,23 @@ Q_FP_TAN        .proc
                 PHP
                 setaxl
                 PHA
+                PHX
                 LDA ARGUMENT1
-                STA SCRATCH
-                LDA ARGUMENT1+2
-                STA SCRATCH+2
-                CALL Q_FP_COS
-                LDA ARGUMENT1
-                PHA
-                LDA ARGUMENT1+2
-                PHA
-                LDA SCRATCH
-                STA ARGUMENT1
-                LDA SCRATCH+2
-                STA ARGUMENT1+2
-                CALL Q_FP_SIN
-                PLA
-                STA ARGUMENT2+2
-                PLA
                 STA ARGUMENT2
-                CALL OP_FP_DIV
+                LDA ARGUMENT1+2
+                STA ARGUMENT2+2
+                CALL Q_SQ
+                PHB
+                setas
+                LDA #`tan_coeff
+                PHA
+                PLB
                 setal
+                LDX #<>tan_coeff
+                CALL Q_POLY_HR
+                PLB
+                CALL OP_FP_MUL
+                PLX
                 PLA
                 PLP
                 RETURN
@@ -388,7 +497,7 @@ Q_FP_LN         .proc
                 PLB
                 setal
                 LDX #<>ln_coeff
-                CALL POLY_HR
+                CALL Q_POLY_HR
                 LDA #TYPE_FLOAT
                 STA @l ARGTYPE1
                 STA @l ARGTYPE1
@@ -466,6 +575,28 @@ FP_TAN          .proc
                 CALL Q_FP_SCALE_TAU
                 CALL Q_FP_NORM_ANGLE
                 CALL Q_FP_TAN
+                ;; Compute 1/tan() if argument was reflected about pi/4
+                TXA
+                AND #1
+                BEQ no_inv
+                CALL Q_INV
+no_inv          TXA
+                ;; Check if flag bit 2 and 1 are different; if so, we
+                ;; need to negate the result. The check is done by
+                ;; shifting the flags down two positions, leaving bit2
+                ;; as bit 0 and bit 1 in carry. Then, add #0 with
+                ;; carry. If bit 0 of the result is 1, then bit 2 and
+                ;; 1 were different.
+                setas
+                LSR
+                LSR
+                ADC #0
+                AND #1
+                BEQ no_neg
+                LDA ARGUMENT1+3
+                ORA #$80
+                STA ARGUMENT1+3
+no_neg          setal
                 PLX
                 PLA
                 PLP
@@ -474,8 +605,9 @@ FP_TAN          .proc
 
 FP_LN           .proc
                 CALL Q_FP_LN
+                RETURN
                 .pend
-        
+
 cos_coeff
                 .dword $37D00D01
                 .dword $BAB60B61
@@ -488,6 +620,13 @@ sin_coeff
                 .dword $B9500D01
                 .dword $3C088889
                 .dword $BE2AAAAB
+                .dword $3F800000
+
+tan_coeff
+                .dword $3CB327A4
+                .dword $3D5D0DD1
+                .dword $3E088889
+                .dword $3EAAAAAB
                 .dword $3F800000
 
 ln_coeff        .dword $3DE38E39
